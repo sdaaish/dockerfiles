012ac49d53525ee0e973436b786807d703f6dbd5
Stig Dahl <stig@charlottendal.net>
2021-06-04 00:46:47 +0200

    Use multistage build

--- sublist3r/build.ps1
+++ sublist3r/build.ps1
@@ -5,6 +5,4 @@ param(
 $image = $name + ":" + $tag
 $image
 
-git clone https://github.com/aboul3la/Sublist3r.git src
 docker image build --no-cache --force-rm --pull --tag $image .
-Remove-Item -Recurse -Force src
