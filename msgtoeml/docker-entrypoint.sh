162e03a60494cbab2ff5bfe711ae08ba6e2a477f
Stig Dahl <stig@charlottendal.net>
2025-07-04 17:41:40 +0200

    Update to a moderns version

--- msgtoeml/docker-entrypoint.sh
+++ msgtoeml/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-$i=0
-for file in $(ls -1 *.msg)
+# Convert all .msg files and save them as .eml
+
+for file in *.msg
 do
-    newfile="$((i++)).eml"
+    newfile="${file%%.msg}.eml"
     printf "Converting ${file} to ${newfile}"
     msgconvert --outfile "${newfile}" "${file}"
 done
